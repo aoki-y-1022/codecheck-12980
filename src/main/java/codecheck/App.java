745c1f470f390075e97a38a570ebeee0e6d7ac48
aoki <tldgr312boc@gmail.com>
2017-06-07 02:43:56 +0900

    no message

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,9 +2,25 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+			System.out.println(makeSeries(Integer.valueOf(args[0])));
+		}
+	
+
+	private static String makeSeries(int num){
+		if(num == 1){
+			return "1";
+		}else{
+			String str = String.valueOf(makeSeries(num-1));
+			int i=0;
+			String answer = "";
+			while(i < str.length()){
+				int j=0;
+				while(i+(++j) <str.length() && str.charAt(i+j) == str.charAt(i)){}
+				answer = answer + String.valueOf(j) + str.charAt(i);
+				i = i + j;
+			}
+			
+			return answer;
 		}
 	}
 }
